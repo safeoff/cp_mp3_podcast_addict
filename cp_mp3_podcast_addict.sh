ddb1ced496d5675d12c326ff16b61ddb601beffd
ry <sgmrrc@gmail.com>
2018-05-24 22:20:08 +0900

    e

--- cp_mp3_podcast_addict.sh
+++ cp_mp3_podcast_addict.sh
@@ -4,6 +4,7 @@
 y="~/storage/shared/Android/data/com.bambuna.podcastaddict/files/podcast/"
 p=("Misreading Chat" "Turing Complete FM" "dex.fm" "Rebuild")
 q="~/storage/shared/Music/mp3data"
+k="~/storage/shared/app/cp_mp3_podcast_addict/"
 
 # 重複チェック
 IFS=$'\n'
@@ -13,19 +14,19 @@ for n in "${p[@]}"; do
     fi
 done
 if [ -e a ]; then
-    c=`sort a ~/storage/shared/app/cp_mp3_podcast_addict/ignore.txt | uniq -u`
+    c=`sort a ~${k}ignore.txt | uniq -u`
     rm a
 fi
 
 # コピーする・ignore.txtに書き出す
 for d in $c; do
     cp $d $q/`basename $d`
-    echo "$d" >> ~/storage/shared/app/cp_mp3_podcast_addict/ignore.txt
+    echo "$d" >> ${k}ignore.txt
     echo $d
 done
 
 # ignore.txtの行数を制限する
 if [ `cat ignore.txt | wc -l` -gt 10 ] ; then
-    sed -e '11,$d' ~/storage/shared/app/cp_mp3_podcast_addict/ignore.txt > e
-    mv e ~/storage/shared/app/cp_mp3_podcast_addict/ignore.txt
+    sed -e '11,$d' ${k}ignore.txt > e
+    mv e ${k}ignore.txt
 fi
